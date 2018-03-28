b227e34bb9e445cf62af8e355804902f8ae95111
girikalra <girikalra@gmail.com>
2018-03-27 20:06:06 -0700

    finish redis setup

--- server/routers/restaurants_api.js
+++ server/routers/restaurants_api.js
@@ -2,25 +2,10 @@ var express = require('express');
 var router = express.Router();
 var getRestaurantById = require('../../db/controllers/getRestaurantById.js');
 database = process.env.TOGGLE || 'mongo';
+var cache = require('express-redis-cache')();
+cacheTime = process.env.CACHE_TIME || 500;
 
-const redis = require('redis');
-const REDIS_PORT = process.env.REDIS_PORT;
-
-const client = redis.createClient(REDIS_PORT);
-
-function cache(req, res, next) {
-    const org = req.query.org;
-    client.get(org, function (err, data) {
-        if (err) throw err;
-        if (data != null) {
-            res.send(respond(org, data));
-        } else {
-            next();
-        }
-    });
-}
-
-router.get('/:id/sidebar', (req, res) => {
+router.get('/:id/sidebar', cache.route(cacheTime), (req, res) => {
   var restaurantId = req.params.id;
   
   if (database === 'mysql') {
